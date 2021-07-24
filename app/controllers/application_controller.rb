ea458b30fb0a3fa9334300a666cc052dccb81f9f
Tatsuki Nagaoka <nagaoka@mbp.local>
2021-07-24 12:00:06 +0900

    devise を導入する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+        include DeviseTokenAuth::Concerns::SetUserByToken
+
+        helper_method :current_user, :user_signed_in?
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.hosts << "api"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
