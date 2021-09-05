445629631fa14b8a8362dac6ae667415f19b8d10
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-05 12:15:17 +0900

    Remove: posts 関連リソースを削除する

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_025120) do
-
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_09_05_031021) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
