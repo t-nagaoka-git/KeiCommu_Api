d58f62cd80cbd8b2df9365615e27d50941028762
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-11 20:09:01 +0900

    Add: Micropost 作成 API を追加する

--- app/controllers/api/v1/microposts_controller.rb
+++ app/controllers/api/v1/microposts_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::MicropostsController < ApplicationController
+  def create
+    @micropost = User.find(params[:user_id]).microposts.build(micropost_params)
+    @micropost.save!
+  end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
 
-        helper_method :current_user, :user_signed_in?
+  helper_method :current_user, :user_signed_in?
+
+  before_action :set_default_response
+
+  def set_default_response
+    @result = true
+    @status = 200
+    @message = ""
+    @total = 0
+  end
 end
