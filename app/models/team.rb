10f45f634cbfd213fee69fdabeb99a0e5c142a25
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-10-23 11:51:01 +0900

    Add: Team モデルに visibility_setting と gender_restriction の enum を追加する

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,7 @@ class Team < ApplicationRecord
   has_many :team_users, dependent: :destroy
   has_many :team_messages, dependent: :destroy
   has_many :users, through: :team_users
+
+  enum visibility_setting: { close: 0, open: 1 }, _prefix: true
+  enum gender_restriction: { none: 0, man: 1, woman: 1 }, _prefix: true
 end
