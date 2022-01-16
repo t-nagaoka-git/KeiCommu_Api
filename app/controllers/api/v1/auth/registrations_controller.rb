0d5b8a6d0701c6c889782b1180df7e9f0fc4e311
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2022-01-17 08:52:44 +0900

    Add: ユーザー編集で許可するパラメーターを設定する

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
     def sign_up_params
       params.permit(:email, :password, :password_confirmation, :name, :gender)
     end
+
+    def account_update_params
+      params.require(:registration).permit(:email, :name, :description)
+    end
 end
