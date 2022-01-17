9a5be4b4bb660e084d7bfc1ac817aa033b047343
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2022-01-18 08:56:49 +0900

    Add: User モデルと Relationship の association を追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
   has_many :team_users, dependent: :destroy
   has_many :team_messages, dependent: :destroy
   has_many :teams, through: :team_users
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
