314d7119ef7a1fc50c11dc60783283706ca9e387
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2022-01-19 08:55:08 +0900

    Fix: RelationshipsController のパスを変更する

--- test/controllers/api/v1/relationships_controller_test.rb
+++ test/controllers/api/v1/relationships_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RelationshipsControllerTest < ActionDispatch::IntegrationTest
+class Api::V1::RelationshipsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
