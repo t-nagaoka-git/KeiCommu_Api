7a39f7e052a802b23172c5216492f698e4cbc59e
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2022-01-24 22:18:33 +0900

    Fix: RelationshipsController の create と destroy アクションの引数名間違いを修正する

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::RelationshipsController < ApplicationController
 
   def create
     @user = User.find(params[:followed_id])
-    current_api_v1_user.follow(user)
+    current_api_v1_user.follow(@user)
   end
 
   def destroy
     @user = User.find(params[:followed_id])
-    current_api_v1_user.unfollow(user)
+    current_api_v1_user.unfollow(@user)
   end
 end
